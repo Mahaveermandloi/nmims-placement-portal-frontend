81c185adccce270fd4627c5ed6664bd0fb4f882d
Mahaveer Mandloi <132800572+Mahaveermandloi@users.noreply.github.com>
2026-02-03 22:53:22 +0530

    Update URLPath.jsx

--- src/Utils/URLPath.jsx
+++ src/Utils/URLPath.jsx
@@ -3,8 +3,8 @@ export const ADMIN_PATH = "/nmims/admin";
 // export const ADMIN_PATH = "/admin";
 export const STUDENT_PATH = "/student";
 
-export const SERVER_URL = "http://localhost:8000";
-// export const SERVER_URL = "https://nmims-placement-portal-backend.vercel.app";
+// export const SERVER_URL = "http://localhost:8000";
+export const SERVER_URL = "https://nmims-placement-portal-backend-6.onrender.com"
 
 // export const SERVER_URL = "https://nmims-placement-website.vercel.app";
 // export const SERVER_URL = "https://nmims-placement-portal.vercel.app";
